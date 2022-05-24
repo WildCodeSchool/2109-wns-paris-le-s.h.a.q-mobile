fa13c3fcb292ba787e4ccf67c4397d1901db0b26
Quentin-HOCKING <hocking.quentin@gmail.com>
2022-05-24 17:37:45 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { extendTheme, NativeBaseProvider } from "native-base";
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+const theme = extendTheme({
+  colors: {
+    // Add new color
+    primary: {
+      50: "#E3F2F9",
+      100: "#C5E4F3",
+      200: "#A2D4EC",
+      300: "#7AC1E4",
+      400: "#47A9DA",
+      500: "#0088CC",
+      600: "#007AB8",
+      700: "#006BA1",
+      800: "#005885",
+      900: "#003F5E",
+    },
+    // Redefinig only one shade, rest of the color will remain same.
+    amber: {
+      400: "#d97706",
+    },
+  },
+  background: {
+    paper: "#2A167A",
+    default: "#2A167A",
+  },
+  config: {
+    // Changing initialColorMode to 'dark'
+    initialColorMode: "dark",
+  },
+});
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <NativeBaseProvider theme={theme}>
+        <SafeAreaProvider>
+          <Navigation />
+          <StatusBar />
+        </SafeAreaProvider>
+      </NativeBaseProvider>
+    );
+  }
+}
---
+++ navigation/LinkingConfiguration.ts
@@ -0,0 +1,55 @@
+/**
+ * Learn more about deep linking with React Navigation
+ * https://reactnavigation.org/docs/deep-linking
+ * https://reactnavigation.org/docs/configuring-links
+ */
+
+import { LinkingOptions } from "@react-navigation/native";
+import * as Linking from "expo-linking";
+
+import { RootStackParamList } from "../types";
+
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: [Linking.makeUrl("/")],
+  config: {
+    screens: {
+      Home: {
+        screens: {
+          Login: {
+            screens: {
+              LoginScreen: "login",
+            },
+          },
+
+        },
+      },
+      SignUp: {
+        screens: {
+          SignUp: {
+            screens:{
+              SignUpScreen:"signUp",
+            },
+          },
+        },
+      },
+      Tasks: {
+        screens: {
+          TabOne: {
+            screens: {
+              TabOneScreen: "one",
+            },
+          },
+          TabTwo: {
+            screens: {
+              TabTwoScreen: "two",
+            },
+          },
+        },
+      },
+      Modal: "modal",
+      NotFound: "*",
+    },
+  },
+};
+
+export default linking;
